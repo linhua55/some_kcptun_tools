626897d1ca0262b25c27e56bb2a905bc8b819023
linhua <5linhua5@gmail.com>
2016-12-26 15:48:21 +0800

    Monitor the traffic to change port

--- udpRelayClient.py
+++ udpRelayClient.py
@@ -21,19 +21,24 @@ buffer = 32768
 class UdpRelayClient(object):
     def udpRelayClient(self):
         outBytes = 0
+        inBytes = 0
 
         clientSocketRelay = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         clientSocketRelay.bind(('', clientRelayPort))
         clientSocketOut = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         clientSocketIn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        clientSocketIn2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
         message_queues = {}
         message_queues[clientSocketOut] = queue.Queue()
-        message_queues[clientSocketIn] = queue.Queue()
         message_queues[clientSocketRelay] = queue.Queue()
         while True:
             sleep(0.001)
-            self.ready_socks, self.writable_socks, _ = select.select([clientSocketRelay, clientSocketIn], [clientSocketOut, clientSocketRelay], [])
+            # TODO timeout parameter  When message_queues is null, execute this(only InSocket), to reduce cpu usage
+            self.ready_socks, self.writable_socks, _ = select.select([clientSocketRelay, clientSocketIn2, clientSocketIn], [clientSocketOut, clientSocketRelay], [])
+            # self.ready_socks = None
+            # if message_queues[clientSocketOut].qsize() == 0 or message_queues[clientSocketRelay].qsize() == 0:
+            #     self.ready_socks, self.writable_socks, _ = select.select([clientSocketRelay, clientSocketIn, clientSocketIn2], [clientSocketOut, clientSocketRelay], [])
             for sock in self.ready_socks:
                 if sock.fileno() == clientSocketRelay.fileno():
                     try:
@@ -49,10 +54,19 @@ class UdpRelayClient(object):
                             # TODO
                             clientSocketOut = self.recreateSocket(clientSocketOut, message_queues)
 
+                # resolve the reside traffic when clientSocketIn changed(change port)
+                # before clientSocketIn(Wrong!!!not useful)
+                elif sock.fileno() == clientSocketIn2.fileno():
+                    backRecvData = sock.recv(buffer)
+                    if backRecvData:
+                        message_queues[clientSocketRelay].put(backRecvData)
+
                 elif sock.fileno() == clientSocketIn.fileno():
                     backRecvData = sock.recv(buffer)
                     if backRecvData:
                         message_queues[clientSocketRelay].put(backRecvData)
+                        inBytes = inBytes + len(backRecvData)
+
 
 
             for sock in self.writable_socks:
@@ -74,13 +88,22 @@ class UdpRelayClient(object):
                     else:
                         sock.sendto(next_msg, (localAddress, localPort))
 
-            # if outBytes > 10M, change a sending port.  outBytes == serverInboundBytes
-            if outBytes > 10*1024*1024:
+            # if outBytes > 5M, change a sending port.  outBytes == serverInboundBytes
+            if outBytes > 5*1024*1024:
                 outBytes = 0
                 clientSocketOut = self.recreateSocket(clientSocketOut, message_queues)
 
+            #  DONE !!!need to check whether or not the clientSocketIn have data to be read!!!
+            #  and (clientSocketIn not in self.ready_socks)
+            # if inBytes > 10M, chang a port  !! Control the traffic or control the speed
+            if inBytes > 10*1024*1024:
+                inBytes = 0
+                clientSocketIn2 = clientSocketIn
+                clientSocketIn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
         clientSocketOut.close()
         clientSocketIn.close()
+        clientSocketIn2.close()
         clientSocketRelay.close()
 
 
